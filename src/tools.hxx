e4609b1c735beb6bdcb79523f8b6c6b10b83d11f
Elkeid-me <elkeid.me@gmail.com>
2023-12-09 23:47:22 +0800

    添加 `logs::error_no_exit`；为 `logs::debug` 添加包装宏；尝试在 `logs::error` 使用异常机制进行栈回溯

--- src/tools.hxx
+++ src/tools.hxx
@@ -3,7 +3,15 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <cstdlib>
 #include <iostream>
+
+enum class exceptions
+{
+    GENERAL_EXCEPTION
+};
+
+#define log_debug(...) ::logs::debug(__VA_ARGS__)
 
 namespace logs
 {
@@ -17,11 +25,19 @@ namespace logs
         ((std::cerr << "\033[32;1m[DEBUG]\033[0m ") << ... << args) << '\n';
     }
 
-    template <typename... V> void error(V... args)
+    template <typename... V> void error_no_exit(V... args)
     {
         ((std::cerr << "\033[31;1m[ERROR]\033[0m ") << ... << args) << '\n';
     }
+
+    template <typename... V> void error(V... args)
+    {
+        error_no_exit(args...);
+        log_debug("抛出 `exceptions::GENERAL_EXCEPTION`, 开始栈回溯");
+        throw exceptions::GENERAL_EXCEPTION;
+    }
 }
+
 
 std::uint32_t compute_checksum(const void *pkt, std::size_t n_bytes);
 
