4b70453bb61dfad734f287d71d29aa950432113e
Elkeid-me <elkeid.me@gmail.com>
2023-12-12 14:23:39 +0800

    更新套接字的延时设置

--- src/socket_process.cxx
+++ src/socket_process.cxx
@@ -6,6 +6,49 @@
 #include <sys/time.h>
 
 static addrinfo *get_addr_info(const char *host, const char *service,
+                               const addrinfo *hints);
+
+static void set_socket_option(int s, int level, int optname, const void *optval,
+                              int optlen);
+
+static int open_receiver_socket(const char *port);
+
+static int open_sender_socket(const char *hostname, const char *port);
+
+namespace socket_process
+{
+    void set_100ms_recv_timeout(int socket)
+    {
+        constexpr timeval SO_RCVTIMEO_OPTVAL_100ms{0, 100'000};
+        set_socket_option(socket, SOL_SOCKET, SO_RCVTIMEO, &SO_RCVTIMEO_OPTVAL_100ms,
+                          sizeof(timeval));
+    }
+
+    void set_2s_recv_timeout(int socket)
+    {
+        constexpr timeval SO_RCVTIMEO_OPTVAL_2s{2};
+        set_socket_option(socket, SOL_SOCKET, SO_RCVTIMEO, &SO_RCVTIMEO_OPTVAL_2s,
+                          sizeof(timeval));
+    }
+
+    int open_receiver_socket(const char *port)
+    {
+        int ret{::open_receiver_socket(port)};
+        if (ret < 0)
+            error_process::unix_error("`open_receiver_socket()` 错误: ");
+        return ret;
+    }
+
+    int open_sender_socket(const char *host_name, const char *port)
+    {
+        int ret{::open_sender_socket(host_name, port)};
+        if (ret < 0)
+            error_process::unix_error("`open_sender_socket()` 错误: ");
+        return ret;
+    }
+}
+
+static addrinfo *get_addr_info(const char *host, const char *service,
                                const addrinfo *hints)
 {
     addrinfo *result;
@@ -26,7 +69,6 @@ static void set_socket_option(int s, int level, int optname, const void *optval,
 }
 
 static constexpr int SO_REUSEADDR_OPTVAL{1};
-static constexpr timeval SO_RCVTIMEO_OPTVAL{0, 100'000};
 
 static int open_receiver_socket(const char *port)
 {
@@ -52,8 +94,7 @@ static int open_receiver_socket(const char *port)
                 set_socket_option(receiver_fd, SOL_SOCKET, SO_REUSEADDR,
                                   &SO_REUSEADDR_OPTVAL, sizeof(int));
 
-                set_socket_option(receiver_fd, SOL_SOCKET, SO_RCVTIMEO,
-                                  &SO_RCVTIMEO_OPTVAL, sizeof(timeval));
+                socket_process::set_100ms_recv_timeout(receiver_fd);
                 if (bind(receiver_fd, ptr->ai_addr, ptr->ai_addrlen) == 0)
                     break;
 
@@ -91,8 +132,8 @@ static int open_sender_socket(const char *hostname, const char *port)
             sender_fd = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
             if (sender_fd >= 0)
             {
-                set_socket_option(sender_fd, SOL_SOCKET, SO_RCVTIMEO, &SO_RCVTIMEO_OPTVAL,
-                                  sizeof(timeval));
+                socket_process::set_100ms_recv_timeout(sender_fd);
+
                 if (connect(sender_fd, ptr->ai_addr, ptr->ai_addrlen) == 0)
                     break;
                 file_process::close(sender_fd);
@@ -106,23 +147,4 @@ static int open_sender_socket(const char *hostname, const char *port)
     }
 
     return -1;
-}
-
-namespace socket_process
-{
-    int open_receiver_socket(const char *port)
-    {
-        int ret{::open_receiver_socket(port)};
-        if (ret < 0)
-            error_process::unix_error("`open_receiver_socket()` 错误: ");
-        return ret;
-    }
-
-    int open_sender_socket(const char *host_name, const char *port)
-    {
-        int ret{::open_sender_socket(host_name, port)};
-        if (ret < 0)
-            error_process::unix_error("`open_sender_socket()` 错误: ");
-        return ret;
-    }
 }
--- src/socket_process.hxx
+++ src/socket_process.hxx
@@ -5,6 +5,8 @@ namespace socket_process
 {
     int open_receiver_socket(const char *port);
     int open_sender_socket(const char *host_name, const char *port);
+    void set_100ms_recv_timeout(int socket);
+    void set_2s_recv_timeout(int socket);
 }
 
 #endif
