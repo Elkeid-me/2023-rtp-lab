50e9a0654e15c5f32ebd81f2501fb55e1dc3b057
Elkeid-me <elkeid.me@gmail.com>
2023-12-12 15:40:27 +0800

    修复 sender 的重试处理，修复拼写错误

--- src/sender.cxx
+++ src/sender.cxx
@@ -25,17 +25,20 @@ int main(int argc, char **argv)
                 "参数错误. 你可以这样使用: ", argv[0],
                 " [receiver ip] [receiver port] [file path] [window size] [mode]");
 
-        const char *hose_name{argv[1]}, *port{argv[2]}, *file_path{argv[3]};
+        const char *host_name{argv[1]}, *port{argv[2]}, *file_path{argv[3]};
 
         auto [window_size, mode]{parse_window_size_and_mode(argv[4], argv[5])};
 
-        log_debug("接收端地址: ", hose_name);
+        log_debug("接收端地址: ", host_name);
         log_debug("接收端端口: ", port);
         log_debug("文件路径: ", file_path);
         log_debug("窗口大小: ", window_size);
-        log_debug("模式: ", static_cast<int>(mode));
+        log_debug("模式: ", mode);
 
-        sender_main_function(hose_name, port, file_path, window_size, mode);
+        sender_main_function(host_name, port, file_path, window_size, mode);
+
+        log_debug("Sender: 退出");
+        return 0;
     }
     catch (exceptions)
     {
@@ -57,45 +60,51 @@ void handshake(int fd)
 
     {
         const rtp_header header_syn(seq_num, 0, SYN);
-        for (int times{1}; times <= 50; times++)
+        int times{0};
+        for (times = 1; times <= 50; times++)
         {
             if (header_syn.send(fd) == -1)
                 error_process::unix_error("`send()` error: ");
 
-            log_debug("成功发送 ", header_syn);
+            log_debug("\033[33m发送包\033[0m:", '\n', header_syn);
             if (header_buffer.recv(fd) == -1 && (errno == EAGAIN || errno == EWOULDBLOCK))
             {
-                log_debug("接受失败. 当前尝试次数: ", times);
+                log_debug("接收失败. 当前尝试次数: ", times);
                 continue;
             }
 
-            log_debug("接收到 ", header_buffer);
+            log_debug("\033[34m接收包\033[0m:", '\n', header_buffer);
             if (header_buffer.is_valid() && header_buffer.get_flag() == (SYN | ACK) &&
                 header_buffer.get_seq_num() == seq_num + 1)
             {
-                log_debug("包合法. 发送下一个包");
+                log_debug("包合法. 发送 ACK");
                 break;
             }
             log_debug("包不合法. 当前尝试次数: ", times);
         }
+        if (times > 50)
+            return;
     }
 
     {
         socket_process::set_2s_recv_timeout(fd);
         const rtp_header header_ack(seq_num + 1, 0, ACK);
-        for (int times{1}; times <= 50; times++)
+        int times{0};
+        for (times = 1; times <= 50; times++)
         {
             if (header_ack.send(fd) == -1)
                 error_process::unix_error("`send()` error: ");
 
-            log_debug("成功发送 ", header_ack);
+            log_debug("\033[33m发送包\033[0m:", '\n', header_ack);
 
             if (header_buffer.recv(fd) == -1 && (errno == EAGAIN || errno == EWOULDBLOCK))
             {
-                log_debug("2s 延时已过, 可以认为 receiver 收到 ACK");
+                log_debug("2s 延时已过, 可以认为 receiver 收到 ACK. 开始发送文件");
                 break;
             }
         }
+        if (times > 50)
+            return;
         socket_process::set_100ms_recv_timeout(fd);
     }
 }
