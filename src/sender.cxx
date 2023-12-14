955ac651b9223fea2fda6a398b0936f7f2928ac9
Elkeid-me <elkeid.me@gmail.com>
2023-12-14 21:45:38 +0800

    sender 的选择重传算法完成

--- src/sender.cxx
+++ src/sender.cxx
@@ -1,22 +1,41 @@
+#include "error_process.hxx"
 #include "file_process.hxx"
 #include "rtp_header.hxx"
 #include "socket_process.hxx"
 #include "tools.hxx"
+#include <cmath>
+#include <cstddef>
 #include <cstdint>
 #include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <filesystem>
+#include <fstream>
+#include <limits>
 #include <random>
 #include <sys/epoll.h>
 #include <sys/socket.h>
+#include <sys/timerfd.h>
+#include <vector>
 
 void sender_core_function(const char *hose_name, const char *port, const char *file_path,
                           std::size_t window_size, mode_type mode);
 void handshake(int fd, std::uint32_t seq_num);
 void terminate_connection(int fd, std::uint32_t fin_seq_num);
+void send_file_selective_repeat(int fd, const char *file_path, std::size_t window_size,
+                                std::uint32_t window_start_seq_num);
+void resend_selective_repeat(int fd);
+[[nodiscard]] bool process_ack_selective_repeat(std::uint32_t seq_num, int fd);
+
+void send_k(int fd);
+
+void set_timer(int time);
 
 int main(int argc, char **argv)
 {
     try
     {
+        // std::ios::sync_with_stdio(false);
         if (argc != 6)
             logs::error(
                 "参数错误. 你可以这样使用: ", argv[0],
@@ -43,13 +62,209 @@ int main(int argc, char **argv)
     }
 }
 
+std::ifstream ifs;
+
+std::vector<rtp_packet> packets_vec;
+std::vector<std::uint8_t> ack_flags_vec;
+
+std::size_t remain_file_size;
+std::size_t n_need_ack_window;
+
+std::size_t window_size;
+std::size_t file_window;
+
+std::size_t window_left_seq_num;
+std::size_t window_right_seq_num;
+std::size_t window_left_unsend_seq_num;
+
+int ep_fd;
+int timer_fd;
+
 void sender_core_function(const char *hose_name, const char *port, const char *file_path,
                           std::size_t window_size, mode_type mode)
 {
     file_process::fd_wrapper socket(socket_process::open_sender_socket(hose_name, port));
-    const std::uint32_t seq_num{std::random_device{}()};
+    file_process::fd_wrapper timer_wrapper(timerfd_create(0, 0));
+    timer_fd = timer_wrapper.get_file_descriptor();
+
+    if (timer_fd == -1)
+        error_process::unix_error("`timerfd_create()` 错误: ");
+    file_process::fd_wrapper epoll_wrapper(epoll_create1(0));
+    ep_fd = epoll_wrapper.get_file_descriptor();
+    if (ep_fd == -1)
+        error_process::unix_error("`epoll_create1()` 错误: ");
+
+    static epoll_event ep_event_sock, ep_event_timer;
+    ep_event_sock.events = EPOLLIN;
+    ep_event_sock.data.fd = socket.get_file_descriptor();
+    if (epoll_ctl(ep_fd, EPOLL_CTL_ADD, socket.get_file_descriptor(), &ep_event_sock) ==
+        -1)
+        error_process::unix_error("`epoll_ctl()` 错误: ");
+
+    ep_event_timer.events = EPOLLIN;
+    ep_event_timer.data.fd = timer_wrapper.get_file_descriptor();
+    if (epoll_ctl(ep_fd, EPOLL_CTL_ADD, timer_wrapper.get_file_descriptor(),
+                  &ep_event_timer) == -1)
+        error_process::unix_error("`epoll_ctl()` 错误: ");
+
+    std::uint32_t seq_num{0}; // {std::random_device{}()};
+    if (seq_num > std::numeric_limits<std::uint16_t>::max())
+        seq_num /= (std::numeric_limits<std::uint8_t>::max() + 1);
     handshake(socket.get_file_descriptor(), seq_num);
-    // terminate_connection(socket.get_file_descriptor(), 0);
+    log_debug("握手完成");
+    send_file_selective_repeat(socket.get_file_descriptor(), file_path, window_size,
+                               seq_num + 1);
+    log_debug("文件发送完成");
+    terminate_connection(socket.get_file_descriptor(), seq_num + 1 + file_window);
+}
+
+void send_file_selective_repeat(int fd, const char *file_path, std::size_t window_size,
+                                std::uint32_t start_seq_num)
+{
+    ifs.open(file_path, std::ios::binary);
+    if (ifs.fail())
+        logs::error("打开文件 `", file_path, "` 时出现了问题");
+
+    remain_file_size = std::filesystem::file_size(file_path);
+    log_debug("文件大小: ", remain_file_size);
+    file_window = remain_file_size / PAYLOAD_MAX;
+
+    if (remain_file_size % PAYLOAD_MAX > 0)
+        file_window += 1;
+    ::window_size = window_size;
+
+    packets_vec.resize(window_size);
+    ack_flags_vec.resize(window_size, false);
+
+    window_left_seq_num = start_seq_num;
+    window_left_unsend_seq_num = window_left_seq_num;
+    window_right_seq_num =
+        window_left_seq_num + (file_window > window_size ? window_size : file_window);
+
+    n_need_ack_window = file_window;
+
+    epoll_event ep_event{EPOLLIN};
+    int attemp_times{0};
+    rtp_header header_buf;
+
+    log_debug("开始发送文件");
+    while (true)
+    {
+        if (n_need_ack_window == 0)
+            return;
+        send_k(fd);
+
+        if (epoll_wait(ep_fd, &ep_event, 1, -1) == -1)
+            error_process::unix_error("`epoll_wait()` 错误: ");
+
+        if (ep_event.data.fd != fd)
+        {
+            attemp_times++;
+            if (attemp_times > 50)
+                logs::error("发送数据达到最大尝试次数");
+            resend_selective_repeat(fd);
+            set_timer(100);
+        }
+        else
+        {
+            if (header_buf.recv(fd) == -1 && (errno != EAGAIN && errno != EWOULDBLOCK))
+                logs::error("接收包发生了错误");
+            if (header_buf.is_valid() && header_buf.get_length() == 0 &&
+                header_buf.get_flag() == ACK)
+            {
+                if (process_ack_selective_repeat(header_buf.get_seq_num(), fd))
+                {
+                    attemp_times = 0;
+                    set_timer(100);
+                }
+            }
+        }
+    }
+}
+
+[[nodiscard]] bool process_ack_selective_repeat(std::uint32_t seq_num, int fd)
+{
+    if (seq_num < window_left_seq_num || seq_num >= window_right_seq_num)
+    {
+        log_debug("`process_ack()`: 接收到的 `seq_num`: ", seq_num, " 超出当前窗口 [",
+                  window_left_seq_num, ", ", window_right_seq_num - 1, ']');
+        return false;
+    }
+
+    if (ack_flags_vec[seq_num % window_size])
+        return false;
+
+    log_debug("ACK ", seq_num);
+    ack_flags_vec[seq_num % window_size] = true;
+
+    if (seq_num != window_left_seq_num)
+        return false;
+
+    std::size_t _1st_nack_pkt;
+    for (_1st_nack_pkt = window_left_seq_num; _1st_nack_pkt < window_right_seq_num;
+         _1st_nack_pkt++)
+    {
+        std::size_t index{_1st_nack_pkt % window_size};
+
+        if (!ack_flags_vec[index])
+            break;
+
+        ack_flags_vec[index] = false;
+    }
+
+    std::size_t difference{_1st_nack_pkt - window_left_seq_num};
+    window_left_seq_num += difference;
+    n_need_ack_window -= difference;
+    window_left_unsend_seq_num = window_right_seq_num;
+    window_right_seq_num += difference;
+    if (window_right_seq_num - window_left_seq_num > n_need_ack_window)
+        window_right_seq_num = window_left_seq_num + n_need_ack_window;
+
+    log_debug("窗口变为 ", window_left_seq_num, ' ', window_right_seq_num, ' ',
+              window_left_unsend_seq_num);
+    return true;
+}
+
+void resend_selective_repeat(int fd)
+{
+    for (std::size_t i{window_left_seq_num}; i < window_right_seq_num; i++)
+    {
+        std::size_t index{i % window_size};
+        if (!ack_flags_vec[index])
+        {
+            // log_debug("重发", packets_vec[index].get_seq_num());
+            if (packets_vec[index].send(fd) == -1)
+                logs::error("发送包失败", packets_vec[index]);
+        }
+    }
+}
+
+void send_k(int fd)
+{
+    for (std::size_t seq_num{window_left_unsend_seq_num}; seq_num < window_right_seq_num;
+         seq_num++)
+    {
+        std::size_t index{seq_num % window_size};
+        std::size_t payload_size{remain_file_size > PAYLOAD_MAX ? PAYLOAD_MAX
+                                                                : remain_file_size};
+        ifs.read(packets_vec[index].get_buf(), payload_size);
+
+        remain_file_size -= payload_size;
+
+        packets_vec[index].make_packet(seq_num, payload_size, 0);
+        if (packets_vec[index].send(fd) == -1)
+            logs::error("发送包失败", packets_vec[index]);
+        // log_debug("发送 ", packets_vec[index].get_seq_num());
+    }
+    window_left_unsend_seq_num = window_right_seq_num;
+    // set_timer(100);
+}
+
+void set_timer(int time)
+{
+    itimerspec spec{{0, 0}, {time / 1000, time % 1000 * 1000'000}};
+    if (timerfd_settime(timer_fd, 0, &spec, NULL) == -1)
+        error_process::unix_error("`timerfd_settime()` 错误: ");
 }
 
 void handshake(int fd, std::uint32_t seq_num)
